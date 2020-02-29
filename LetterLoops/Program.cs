84e8cea3a68819e054a069efb93799ce1e313bf6
Connor Sullivan <cksullvn@gmail.com>
2020-02-29 13:27:42 -0600

    finished letter loops assignment with Jamekas help

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a sequence of letters to see fancy things happen");
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            string letterSequence = Console.ReadLine().ToLower();
+            var builder = "";
+            for (int i = 0; i < letterSequence.Length; i++)
+            {
+                // for each letter/char, write a capital version of that letter, and lowercase versions of that letter multiplied by the index+1
+                var position = i + 1; 
+                for (var k = 0; k < position; k++)
+                {
+                    if (k == 0)
+                    {
+                        char upperCase = Char.ToUpper(letterSequence[i]);
+                        builder += upperCase;
+                    }
+                    else
+                    {
+                        builder += letterSequence[i];
+                    }
+                }
+                builder += "-";
+            }
+            char[] charsToTrim1 = { '-' };
+            
+            Console.WriteLine(builder.TrimEnd(charsToTrim1));
+        }
+    }
+}
